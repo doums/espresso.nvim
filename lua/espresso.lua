20e20eea9c7dea979b68e9a862bab480f26616d7
pierre <dommerc.pierre@gmail.com>
2021-04-14 01:33:14 +0200

    wip

--- lua/espresso.lua
+++ lua/espresso.lua
@@ -28,91 +28,74 @@ g.colors_name = 'espresso'
 
 -- the palette
 local p = {
-  null = 'NONE',
-  bg = '#2B2B2B',
-  fg = '#A9B7C6',
-  cursor = '#BBBBBB',
-  identifierUnderCaret = '#344134',
-  identifierUnderCaretWrite = '#40332B',
-  gutter = '#313335',
-  selection = '#214283',
-  cursorLine = '#323232',
+  bg = '#2A211C',
+  fg = '#BDAE9D',
+  cursor = '#889AFF',
+  identifierUnderCaret = '#3C3C57',
+  identifierUnderCaretWrite = '#472C47',
+  gutter = '#2A211C',
+  selection = '#95B8FA',
+  cursorLine = '#3A312C',
   cursorLineNr = '#A4A3A3',
   errorMsg = '#CC666E',
   error = '#BC3F3C',
-  warning = '#A9B7C6',
-  muted = '#606060',
-  link = '#287BDE',
-  stdOutput = '#BBBBBB',
-  lineNumber = '#606366',
-  matchBraceFg = '#FFEF28',
-  matchBraceBg = '#3B514D',
-  todo = '#A8C023',
-  search = '#32593D',
-  incSearch = '#155221',
+  warning = '#4A3F10',
+  muted = '#BFBFBF',
+  link = '#C7C7FF',
+  stdOutput = '#E4E4FF',
+  lineNumber = '#BDAE9D',
+  matchBrace = '#3A6DA0',
+  todo = '#C7C7FF',
+  search = '#5F5F00',
+  incSearch = '#4F4F82',
   foldedFg = '#8C8C8C',
   foldedBg = '#3A3A3A',
-  constant = '#9876AA',
-  keyword = '#CC7832',
-  comment = '#808080',
-  docComment = '#629755',
-  string = '#6A8759',
-  number = '#6897BB',
-  delimiter = '#CC7832',
-  specialComment = '#8A653B',
-  ['function'] = '#FFC66D',
+  constant = '#C5656B',
+  keyword = '#43A8ED',
+  comment = '#0066FF',
+  string = '#049B0A',
+  number = '#44AA43',
+  commentTag = '#8A653B',
+  ['function'] = '#FF9358',
   diffAdd = '#294436',
   diffText = '#385570',
   diffDelete = '#484A4A',
   diffChange = '#303C47',
   addStripe = '#384C38',
-  stripeWhiteSpace = '#4C4638',
+  whitespaceStripe = '#4C4638',
   changeStripe = '#374752',
   deleteStripe = '#656E76',
   typo = '#659C6B',
-  metaData = '#BBB529',
+  metaData = '#43A8ED',
   macroName = '#908B25',
   cDataStructure = '#B5B6E3',
-  cStructField = '#9373A5',
   debug = '#666D75',
-  codeError = '#532B2E',
-  codeWarning = '#52503A',
-  errorStripe = '#9E2927',
-  warnStripe = '#BE9117',
-  infoStripe = '#756D56',
-  hintStripe = '#6c7176',
-  typeDef = '#B9BCD1',
+  errorStripe = '#FF0000',
+  warnStripe = '#FFFF00',
+  infoStripe = '#FFFFCC',
+  hintStripe = '#9E9E80',
   menu = '#46484A',
   menuFg = '#BBBBBB',
   menuSel = '#113A5C',
   menuSBar = '#616263',
-  tag = '#E8BF6A',
-  entity = '#6D9CBE',
-  htmlAttribute = '#BABABA',
-  htmlString = '#A5C261',
-  tsObject = '#507874',
   statusLine = '#3C3F41',
   statusLineFg = '#BBBBBB',
   statusLineNC = '#787878',
   tabLineSel = '#4E5254',
-  shCommand = '#C57633',
-  templateLanguage = '#232525',
-  rustMacro = '#4EADE5',
-  rustLifetime = '#20999D',
-  duplicateFromServer = '#5E5339',
+  duplicateFromServer = '#30322B',
   hintBg = '#3B3B3B',
   hintFg = '#787878',
-  wrapGuide = '#2F2F2F',
+  wrapGuide = '#4D4D4D',
   UIBorder = '#616161',
   UISelection = '#0D293E',
   ANSIBlack = '#FFFFFF',
-  ANSIRed = '#FF6B68',
-  ANSIGreen = '#A8C023',
-  ANSIYellow = '#D6BF55',
-  ANSIBlue = '#5394EC',
-  ANSIMagenta = '#AE8ABE',
-  ANSICyan = '#299999',
-  ANSIGray = '#999999',
+  ANSIRed = '#FF6767',
+  ANSIGreen = '#68E868',
+  ANSIYellow = '#754200',
+  ANSIBlue = '#C7C7FF',
+  ANSIMagenta = '#FF2EFF',
+  ANSICyan = '#06B8B8',
+  ANSIGray = '#A7A7A7',
   ANSIDarkGray = '#555555',
   ANSIBrightRed = '#FF8785',
   ANSIBrightGreen = '#A8C023',
@@ -130,7 +113,7 @@ local p = {
 local function hi(name, foreground, background, style)
   local fg = 'guifg='..(foreground or 'NONE')
   local bg = 'guibg='..(background or 'NONE')
-  local decoration = style or 'NONE'
+  local decoration = 'gui='..(style or 'NONE')
   local hi_command = string.format('hi %s %s %s %s', name, fg, bg, decoration)
   cmd(hi_command)
 end
@@ -139,19 +122,23 @@ local function li(target, source)
   cmd(string.format('hi! link %s %s', target, source))
 end
 
+-- helper groups
+hi('Warning', nil, p.warning)
+hi('Hint', p.hintFg, p.hintBg)
+
 -- builtin groups `:h highlight-groups`
-hi('ColorColumn', p.null, p.wrapGuide)
+hi('ColorColumn', nil, p.wrapGuide)
 hi('Conceal', p.muted, p.bg)
 hi('Cursor', p.cursor)
 li('lCursor', 'Cursor')
 li('CursorIM', 'Cursor')
 li('CursorColumn', 'CursorLine')
-hi('CursorLine', p.null, p.cursorLine, 'NONE')
+hi('CursorLine', nil, p.cursorLine, 'NONE')
 li('Directory', 'NormalFg')
-hi('DiffAdd', p.null, p.diffAdd)
-hi('DiffChange', p.null, p.diffChange)
-hi('DiffDelete', p.null, p.diffDelete)
-hi('DiffText', p.null, p.diffText)
+hi('DiffAdd', nil, p.diffAdd)
+hi('DiffChange', nil, p.diffChange)
+hi('DiffDelete', nil, p.diffDelete)
+hi('DiffText', nil, p.diffText)
 li('EndOfBuffer', 'NonText')
 -- TermCursor
 -- TermCursorNC
@@ -159,12 +146,12 @@ hi('ErrorMsg', p.errorMsg)
 hi('VertSplit', p.muted)
 hi('Folded', p.foldedFg, p.foldedBg)
 li('FoldColumn', 'Folded')
-hi('SignColumn', p.null, p.gutter)
-hi('IncSearch', p.null, p.incSearch)
+hi('SignColumn', nil, p.gutter)
+hi('IncSearch', nil, p.incSearch)
 li('Substitute', 'Search')
 hi('LineNr', p.lineNumber, p.gutter)
-hi('CursorLineNr', p.cursorLineNr, p.cursorLine)
-hi('MatchParen', p.matchBraceFg, p.matchBraceBg, 'bold')
+hi('CursorLineNr', p.constant, p.bg, 'bold')
+hi('MatchParen', nil, p.matchBrace)
 hi('ModeMsg', p.stdOutput)
 li('MsgArea', 'NormalFg')
 li('MsgSeparator', 'StatusLine')
@@ -179,9 +166,9 @@ hi('PmenuSbar', p.menu, p.menu)
 hi('PmenuThumb', p.menuSBar, p.menuSBar)
 li('Question', 'NormalFg')
 li('QuickFixLine', 'NormalFg')
-hi('Search', p.null, p.search)
+hi('Search', nil, p.search)
 li('SpecialKey', 'NonText')
-hi('SpellBad', p.typo, p.null, 'underline')
+hi('SpellBad', p.typo, nil, 'underline')
 li('SpellCap', 'SpellBad')
 li('SpellLocal', 'SpellBad')
 li('SpellRare', 'SpellBad')
@@ -191,28 +178,28 @@ hi('TabLine', p.statusLineFg, p.statusLine)
 hi('TabLineFill', p.statusLine, p.statusLine)
 hi('TabLineSel', p.fg, p.tabLineSel)
 li('Title', 'Special')
-hi('Visual', p.null, p.selection)
+hi('Visual', nil, p.selection)
 li('VisualNOS', 'Visual')
-hi('WarningMsg', p.warning)
+li('WarningMsg', 'Warning')
 li('Whitespace', 'NonText')
 li('WildMenu', 'PmenuSel')
 
 -- common groups `:h group-name`
-hi('Comment', p.comment)
-hi('Constant', p.constant, p.null, 'italic')
+hi('Comment', p.comment, nil, 'italic')
+hi('Constant', p.constant, nil, 'bold')
 hi('String', p.string)
 li('Character', 'String')
 hi('Number', p.number)
 li('Boolean', 'Keyword')
 li('Float', 'Number')
 li('Identifier', 'NormalFg')
-hi('Function', p['function'])
+hi('Function', p['function'], nil, 'bold')
 li('Statement', 'Keyword')
 li('Conditional', 'Keyword')
 li('Repeat', 'Keyword')
 li('Label', 'Keyword')
 li('Operator', 'Keyword')
-hi('Keyword', p.keyword)
+hi('Keyword', p.keyword, nil, 'bold')
 li('Exception', 'Keyword')
 hi('PreProc', p.metaData)
 li('Include', 'PreProc')
@@ -222,42 +209,42 @@ li('PreCondit', 'PreProc')
 li('Type', 'Keyword')
 li('StorageClass', 'Keyword')
 li('Structure', 'Keyword')
-hi('Typedef', p.typeDef)
+li('Typedef', 'Keyword')
 li('Special', 'PreProc')
 li('SpecialChar', 'PreProc')
 li('Tag', 'Keyword')
-hi('Delimiter', p.delimiter)
-hi('SpecialComment', p.specialComment, p.null, 'italic')
-hi('Debug', p.debug, p.null, 'italic')
+li('Delimiter', 'Keyword')
+hi('SpecialComment', p.commentTag, nil, 'italic')
+hi('Debug', p.debug, nil, 'italic')
 -- Ignore
-hi('Underlined', p.fg, p.null, 'underline')
-hi('Error', p.fg, p.codeError)
-hi('Todo', p.todo, p.null, 'italic')
+hi('Underlined', p.fg, nil, 'underline')
+hi('Error', p.error, p.bg, 'underline')
+hi('Todo', p.todo, nil, 'bold,italic')
 
 -- some other groups
 li('healthSuccess', 'IncSearh')
 hi('NvimInternalError', p.error, p.error)
 hi('RedrawDebugClear', p.fg, p.duplicateFromServer)
 hi('RedrawDebugComposed', p.fg, p.search)
-hi('RedrawDebugRecompose', p.fg, p.codeError)
+li('RedrawDebugRecompose', 'Error')
 
 -- builtin terminal colors
-g.terminal_color_0 = p.ANSIBlack[0]
-g.terminal_color_1 = p.ANSIRed[0]
-g.terminal_color_2 = p.ANSIGreen[0]
-g.terminal_color_3 = p.ANSIYellow[0]
-g.terminal_color_4 = p.ANSIBlue[0]
-g.terminal_color_5 = p.ANSIMagenta[0]
-g.terminal_color_6 = p.ANSICyan[0]
-g.terminal_color_7 = p.ANSIGray[0]
-g.terminal_color_8 = p.ANSIDarkGray[0]
-g.terminal_color_9 = p.ANSIBrightRed[0]
-g.terminal_color_10 = p.ANSIBrightGreen[0]
-g.terminal_color_11 = p.ANSIBrightYellow[0]
-g.terminal_color_12 = p.ANSIBrightBlue[0]
-g.terminal_color_13 = p.ANSIBrightMagenta[0]
-g.terminal_color_14 = p.ANSIBrightCyan[0]
-g.terminal_color_15 = p.ANSIWhite[0]
+g.terminal_color_0 = p.ANSIBlack
+g.terminal_color_1 = p.ANSIRed
+g.terminal_color_2 = p.ANSIGreen
+g.terminal_color_3 = p.ANSIYellow
+g.terminal_color_4 = p.ANSIBlue
+g.terminal_color_5 = p.ANSIMagenta
+g.terminal_color_6 = p.ANSICyan
+g.terminal_color_7 = p.ANSIGray
+g.terminal_color_8 = p.ANSIDarkGray
+g.terminal_color_9 = p.ANSIBrightRed
+g.terminal_color_10 = p.ANSIBrightGreen
+g.terminal_color_11 = p.ANSIBrightYellow
+g.terminal_color_12 = p.ANSIBrightBlue
+g.terminal_color_13 = p.ANSIBrightMagenta
+g.terminal_color_14 = p.ANSIBrightCyan
+g.terminal_color_15 = p.ANSIWhite
 
 -- the following code snippet fix an issue with CursorLine hi group
 -- see https://github.com/neovim/neovim/issues/9019
@@ -274,7 +261,7 @@ li('TSConditional', 'Keyword')
 li('TSConstant', 'Constant')
 li('TSConstBuiltin', 'Keyword')
 li('TSConstMacro', 'cMacroName')
-li('TSError', 'codeError')
+li('TSError', 'Error')
 li('TSException', 'Keyword')
 li('TSField', 'InstanceField')
 li('TSFloat', 'Number')
@@ -286,7 +273,7 @@ li('TSKeyword', 'Keyword')
 li('TSKeywordFunction', 'Keyword')
 li('TSLabel', 'Normal')
 li('TSMethod', 'Function')
-li('TSNamespace', 'cDataStructure')
+li('TSNamespace', 'Normal')
 li('TSNone', 'Normal')
 li('TSNumber', 'Number')
 li('TSOperator', 'Normal')
@@ -301,19 +288,19 @@ li('TSString', 'String')
 li('TSStringRegex', 'Number')
 li('TSStringEscape', 'Keyword')
 li('TSSymbol', 'Identifier')
-li('TSTag', 'htmlTag')
-li('TSTagDelimiter', 'htmlTag')
+li('TSTag', 'Keyword')
+li('TSTagDelimiter', 'Normal')
 li('TSText', 'Normal')
-hi('TSStrong', p.fg, p.null, 'bold')
-hi('TSEmphasis', p.fg, p.null, 'italic')
-hi('TSUnderline', p.fg, p.null, 'underline')
-hi('TSStrike', p.fg, p.null, 'strikethrough')
-hi('TSTitle', p.fg, p.null, 'bold,underline')
+hi('TSStrong', p.fg, nil, 'bold')
+hi('TSEmphasis', p.fg, nil, 'italic')
+hi('TSUnderline', p.fg, nil, 'underline')
+hi('TSStrike', p.fg, nil, 'strikethrough')
+hi('TSTitle', p.fg, nil, 'bold,underline')
 li('TSLiteral', 'Normal')
 li('TSURI', 'markdownLinkText')
 li('TSNote', 'CodeInfo')
-li('TSWarning', 'CodeWarning')
-li('TSDanger', 'CodeError')
+li('TSWarning', 'Warning')
+li('TSDanger', 'Error')
 li('TSType', 'Normal')
 li('TSTypeBuiltin', 'Keyword')
 li('TSVariable', 'Normal')
@@ -323,16 +310,16 @@ li('TSVariableBuiltin', 'Keyword')
 li('LspReferenceText', 'IdentifierUnderCaret')
 li('LspReferenceRead', 'IdentifierUnderCaret')
 li('LspReferenceWrite', 'IdentifierUnderCaretWrite')
-li('LspDiagnosticsDefaultError', 'CodeError')
-li('LspDiagnosticsDefaultWarning', 'CodeWarning')
+li('LspDiagnosticsDefaultError', 'Error')
+li('LspDiagnosticsDefaultWarning', 'TSWarning')
 li('LspDiagnosticsDefaultInformation', 'CodeInfo')
 li('LspDiagnosticsDefaultHint', 'CodeHint')
 li('LspDiagnosticsSignError', 'ErrorSign')
 li('LspDiagnosticsSignWarning', 'WarningSign')
 li('LspDiagnosticsSignInformation', 'InfoSign')
 li('LspDiagnosticsSignHint', 'HintSign')
-li('LspDiagnosticsUnderlineError', 'CodeError')
-li('LspDiagnosticsUnderlineWarning', 'CodeWarning')
+li('LspDiagnosticsUnderlineError', 'Error')
+li('LspDiagnosticsUnderlineWarning', 'Warning')
 li('LspDiagnosticsUnderlineInformation', 'CodeInfo')
 li('LspDiagnosticsUnderlineHint', 'CodeHint')
 li('LspDiagnosticsFloatingError', 'NormalFloat')
